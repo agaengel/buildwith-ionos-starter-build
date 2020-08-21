740507cdc575a8c498daa4d14ac8e93ef7fdb377
hunsalz <>
2020-08-21 13:46:45 +0200

    improved disclaimer

--- src/pages/Disclaimer.vue
+++ src/pages/Disclaimer.vue
@@ -8,28 +8,45 @@
           <a href="https://buildwith.ionos.com/">Buildwith IONOS</a> is bleeding
           edge and is focused on functionality first.
         </p>
-        <ul style="list-style-type:square">
-          <li>UX design guidelines not checked.</li>
-          <li>Legal requirements not checked.</li>
-          <li>
-            No feature completeness. We're far away from any thoughts of
-            completeness. But we think it's time for a friendly user test. We've
-            lot of ideas to improve this tooling. Feel free to share your
-            thoughts with us.
-          </li>
-          <li>
-            All data is volatile. Don't rely on any data.
-          </li>
-          <li>
-            Buildwith IONOS is provided "as is" and you use the application at
-            your own risk.
-          </li>
-          <li>
-            English only - No translations planned. It's a tool for users who
-            are familiar with development tools and the most common tools for
-            developers are English only.
-          </li>
-        </ul>
+        <h2 class="sub-title">Feature set</h2>
+        <p>
+          We are far from feature completeness yet. But we think it's time for a
+          friendly user test. We've lot of ideas to improve this tool. Feel free
+          to share your thoughts with us.
+        </p>
+        <h2 class="sub-title">Limits</h2>
+        <p>
+          You can build max. 25 instances. It doesn't matter if you build 25
+          projects or if you use 25 feature branches of one project.
+        </p>
+        <p>
+          Max. quota of one instance is 500 MB for now.
+        </p>
+        <h2 class="sub-title">UX design</h2>
+        <p>
+          The tool is build with focus on function first by developers. No UX
+          designer was involved so far. We like to ask for your indulgence.
+        </p>
+        <h2 class="sub-title">Data durability</h2>
+        <p>
+          All data is volatile. Don't rely on any data durability with this
+          toolset yet.
+        </p>
+        <h2 class="sub-title">Translation</h2>
+        <p>
+          For now, no translations are planned. It's a tool for users who are
+          familiar with development tools and the most common tools for
+          developers are English only.
+        </p>
+        <h2 class="sub-title">Legal requirements</h2>
+        <p>
+          A legal compliance check is outstanding.
+        </p>
+        <h2 class="sub-title">General</h2>
+        <p>
+          Buildwith IONOS is provided "as is" and you use the application at
+          your own risk.
+        </p>
       </div>
     </section>
   </Layout>
