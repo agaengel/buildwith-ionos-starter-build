85286f8e76b8b566125ba5d44fd35372df5fd2d6
hunsalz <>
2020-08-25 14:47:59 +0200

    changed styling

--- src/pages/Disclaimer.vue
+++ src/pages/Disclaimer.vue
@@ -3,50 +3,54 @@
     <section class="container mx-auto">
       <h1 class="text-5xl">Disclaimer</h1>
       <div class="content">
-        <p>
+        <p class="text-base">
           This version of
           <a href="https://buildwith.ionos.com/">Buildwith IONOS</a> is bleeding
           edge and is focused on functionality first.
         </p>
-        <h2 class="text-3xl">Feature set</h2>
-        <p>
+        <h2 class="text-xl font-medium">Feature set</h2>
+        <div class="mt-2 text-xl leading-6 text-gray-600 pb-4">
           We are far from feature completeness yet. But we think it's time for a
           friendly user test. We've lot of ideas to improve this tool. Feel free
           to share your thoughts with us.
-        </p>
-        <h2 class="text-3xl">Limits</h2>
-        <p>
+        </div>
+        <h2 class="text-xl font-medium">Custom domains &amp; SSL</h2>
+        <div class="mt-2 text-xl leading-6 text-gray-600 pb-4">
+          Custom domains &amp; SSL is not implemented yet.
+        </div>
+        <h2 class="text-xl font-medium">Limits</h2>
+        <div class="mt-2 text-xl leading-6 text-gray-600 pb-4">
           You can build max. 25 instances. It doesn't matter if you build 25
           projects or if you use 25 feature branches of one project.
-        </p>
-        <p>
+        </div>
+        <div class="mt-2 text-xl leading-6 text-gray-600 pb-4">
           Max. quota of one instance is 500 MB for now.
-        </p>
-        <h2 class="text-3xl">UX design</h2>
-        <p>
+        </div>
+        <h2 class="text-xl font-medium">UX design</h2>
+        <div class="mt-2 text-xl leading-6 text-gray-600 pb-4">
           The tool is build with focus on function first by developers. No UX
           designer was involved so far. We like to ask for your indulgence.
-        </p>
-        <h2 class="text-3xl">Data durability</h2>
-        <p>
+        </div>
+        <h2 class="text-xl font-medium">Data durability</h2>
+        <div class="mt-2 text-xl leading-6 text-gray-600 pb-4">
           All data is volatile. Don't rely on any data durability with this
           toolset yet.
-        </p>
-        <h2 class="text-3xl">Translation</h2>
-        <p>
+        </div>
+        <h2 class="text-xl font-medium">Translation</h2>
+        <div class="mt-2 text-xl leading-6 text-gray-600 pb-4">
           For now, no translations are planned. It's a tool for users who are
           familiar with development tools and the most common tools for
           developers are English only.
-        </p>
-        <h2 class="text-3xl">Legal requirements</h2>
-        <p>
+        </div>
+        <h2 class="text-xl font-medium">Legal requirements</h2>
+        <div class="mt-2 text-xl leading-6 text-gray-600 pb-4">
           A legal compliance check is outstanding.
-        </p>
-        <h2 class="text-3xl">General</h2>
-        <p>
+        </div>
+        <h2 class="text-xl font-medium">General</h2>
+        <div class="mt-2 text-xl leading-6 text-gray-600 pb-4">
           Buildwith IONOS is provided "as is" and you use the application at
           your own risk.
-        </p>
+        </div>
       </div>
     </section>
   </Layout>
