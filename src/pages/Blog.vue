d2405c57384e9624a6b26a4dd2b62ff83bf23342
buildwith-ionos <>
2020-09-08 16:42:09 +0200

    layout changes

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -12,7 +12,7 @@
       <div class="content">
         <div class="flex flex-wrap -mx-2">
           <div
-            class="w-full md:w-1/3 mb-8 px-2"
+            class="w-full lg:w-1/3 mb-8 px-2"
             v-for="entry in $page.allBlog.edges"
             :key="entry.node.id"
           >
@@ -31,16 +31,16 @@
               </g-link>
               <!-- card content -->
               <div class="p-8">
-                <h2 class="text-2xl mb-6">
+                <h2>
                   <g-link
                     class="block hover:text-pink-500"
                     :to="entry.node.path"
                     >{{ entry.node.title }}</g-link
                   >
                 </h2>
-                <div class="text-base text-gray-800 md:flex mb-4">
+                <div class="flex flex-col text-base text-gray-800 md:flex mb-4">
                   <div>{{ entry.node.author.name }}</div>
-                  <div class="hidden md:block px-2">—</div>
+                  <div class="hidden md:block px-2"></div>
                   <time :datetime="entry.node.datetime">{{
                     entry.node.humanTime
                   }}</time>
