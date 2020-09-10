80eafe4b22a83075f2dd73807c95b297ac34255f
buildwith-ionos <>
2020-09-10 17:09:11 +0200

    fixed layout of tags and categories

--- src/templates/Tag.vue
+++ src/templates/Tag.vue
@@ -1,28 +1,16 @@
 <template>
   <Layout>
     <section class="container mx-auto">
-      <h1
-        class="text-5xl text-purple-900 text-3xl md:text-center md:text-5xl mb-16 lg:mb-24 lg:text-6xl"
-      >
-        Tag — {{ $page.tag.title }}
-      </h1>
-      <div class="posts">
-        <article
-          class="text-gray-500 mb-8 pb-8 border-b border-gray-200"
-          v-for="element in $page.tag.belongsTo.edges"
-          :key="element.node.id"
-        >
-          <h2 class="text-4xl mb-3">
-            <g-link
-              class="block text-pink-500 hover:text-purple-900"
-              :to="element.node.path"
-              >{{ element.node.title }}</g-link
-            >
-          </h2>
-          <time :datetime="element.node.datetime">{{
-            element.node.humanTime
-          }}</time>
-        </article>
+      <h1>All posts tagged as "{{ $page.tag.title }}"</h1>
+      <div v-for="element in $page.tag.belongsTo.edges" :key="element.node.id">
+        <h2>
+          <g-link
+            class="block text-pink-500 hover:text-purple-900"
+            :to="element.node.path"
+            >{{ element.node.title }}</g-link
+          >
+        </h2>
+        <p :datetime="element.node.datetime">{{ element.node.humanTime }}</p>
       </div>
     </section>
   </Layout>
