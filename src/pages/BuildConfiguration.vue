96b4bad15785ca16a8512405096b83015c5ad702
Alexander Gängel <alexander@gaengel.de>
2020-10-06 22:35:43 +0200

    initial version of BuildConfiguration

---
+++ src/pages/BuildConfiguration.vue
@@ -0,0 +1,23 @@
+<template>
+  <Layout>
+    <section class="container mx-auto">
+      <div class="subheader">
+        <h1>Project Build Configuration</h1>
+        <p>How Buildwith Ionos identifies the project build configuration </p>
+      </div>
+      <div class="content">
+        <p>Buildwith Ionos tries to identify the buildconfiguration for the project that is selected as source.<br>If the configuration cannot be identified, you can select a build template for various type of projects.
+        Currently 
+      </p>
+      </div>
+    </section>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Build Configuration",
+  },
+};
+</script>
